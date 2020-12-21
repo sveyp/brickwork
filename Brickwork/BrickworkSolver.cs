69add698d6c1aba9c831bef4eff944eedad8bb40
svetlomir <petrovsvetlomir@gmail.com>
2020-12-21 23:08:42 +0200

    Brickwork task solution

---
+++ Brickwork/BrickworkSolver.cs
@@ -0,0 +1,89 @@
+﻿namespace Brickwork
+{
+    public class BrickworkSolver
+    {
+        private readonly int[,] inputLayer;
+        private readonly int[,] outputLayer;
+
+        public BrickworkSolver(int[,] inputLayer)
+        {
+            this.inputLayer = inputLayer;
+
+            var n = this.inputLayer.GetLength(0);
+            var m = this.inputLayer.GetLength(1);
+
+            this.outputLayer = new int[n, m];
+        }
+
+        public int[,] Solve()
+        {
+            TryAddBrick(1);
+
+            return outputLayer;
+        }
+
+        private void TryAddBrick(int number)
+        {
+            var n = this.outputLayer.GetLength(0);
+            var m = this.outputLayer.GetLength(1);
+
+            int brickCount = n * m / 2;
+
+            if (number > brickCount)
+            {
+
+                return;
+                // check if we have a solution;
+            }
+            else
+            {
+                // extend the partial solution in every possible ways
+                for (var i = 0; i < n; i++)
+                {
+                    for (var j = 0; j < m; j++)
+                    {
+                        // if can add horizontal brick
+                        var canAddHorizontalBrick = j < m - 1 &&
+                            this.outputLayer[i, j] == 0 &&
+                            this.outputLayer[i, j + 1] == 0 &&
+                            this.inputLayer[i, j] != this.inputLayer[i, j + 1];
+                        var canAddVerticalBrick = i < n - 1 &&
+                            this.outputLayer[i, j] == 0 &&
+                            this.outputLayer[i + 1, j] == 0 &&
+                            this.inputLayer[i, j] != this.inputLayer[i + 1, j];
+
+                        // register solution
+                        if (canAddHorizontalBrick)
+                        {
+                            this.outputLayer[i, j] = number;
+                            this.outputLayer[i, j + 1] = number;
+                        }
+                        else if (canAddVerticalBrick)
+                        {
+                            this.outputLayer[i, j] = number;
+                            this.outputLayer[i + 1, j] = number;
+                        }
+
+                        if (canAddHorizontalBrick || canAddVerticalBrick)
+                        {
+                            TryAddBrick(number + 1);
+                            return;
+                        }
+
+                        // remove the registration
+                        if (canAddHorizontalBrick)
+                        {
+                            this.outputLayer[i, j] = 0;
+                            this.outputLayer[i, j + 1] = 0;
+                        }
+                        else if (canAddVerticalBrick)
+                        {
+                            this.outputLayer[i, j] = 0;
+                            this.outputLayer[i + 1, j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Brickwork/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Brickwork
+{
+    class Program
+    {
+        private static int[,] inputLayer;
+
+        static void Main(string[] args)
+        {
+            var valid = ReadInputLayer();
+            if (!valid)
+            {
+                return;
+            }
+
+            var solver = new BrickworkSolver(inputLayer);
+            var outputLayer = solver.Solve();
+
+            Console.WriteLine("Output layer");
+            Print(outputLayer);
+        }
+        
+
+        private static void Print(int[,] layer)
+        {
+            Console.WriteLine(string.Join("", Enumerable.Repeat('*', layer.GetLength(1) * 3 + 1)));
+            for (var i = 0; i < layer.GetLength(0); i++)
+            {
+                var numericRowFormatted = new StringBuilder();
+                numericRowFormatted.Append('*');
+
+                var delimiterRowFormatter = new StringBuilder();
+                delimiterRowFormatter.Append('*');
+
+                for (var j = 0; j < layer.GetLength(1); j++)
+                {
+                    numericRowFormatted.AppendFormat("{0,2}", layer[i, j]);
+
+                    if (j + 1 < layer.GetLength(1))
+                    {
+                        numericRowFormatted.Append(
+                            layer[i, j] == layer[i, j + 1]
+                                ? ' '
+                                : '*');
+
+                    }
+
+                    if (i + 1 < layer.GetLength(0))
+                    {
+                        if (layer[i, j] == layer[i + 1, j])
+                        {
+                            delimiterRowFormatter.Append("  *");
+                        }
+                        else
+                        {
+                            delimiterRowFormatter.Append("***");
+                        }
+                    }
+                }
+                numericRowFormatted.Append('*');
+                Console.WriteLine(numericRowFormatted.ToString());
+
+                if (i + 1 < layer.GetLength(0))
+                {
+                    Console.WriteLine(delimiterRowFormatter.ToString());
+                }
+            }
+
+            Console.WriteLine(string.Join("", Enumerable.Repeat('*', layer.GetLength(1) * 3 + 1)));
+        }
+        
+
+        private static bool ReadInputLayer()
+        {
+            var dimentionString = Console.ReadLine();
+
+            var dimentions = dimentionString
+                .Split(' ')
+                .Where(x => !string.IsNullOrEmpty(x))
+                .ToList();
+        
+            if (dimentions.Count != 2)
+            {
+                Console.WriteLine("Only 2D array is allowed.");
+                return false;
+            }
+
+            int n = 0;
+            int m = 0;
+            for (int i = 0; i < dimentions.Count; i++)
+            {
+                int dimention;
+
+                string dimentionName = i == 0 ? "N" : "M";
+
+                if (!int.TryParse(dimentions[i], out dimention))
+                {
+                    Console.WriteLine("{0} is not an integer. Please try again.", dimentionName);
+                    return false;
+                }
+
+                if (dimention <= 0)
+                {
+                    Console.WriteLine("{0} should be greater than 0", dimentionName);
+                    return false;
+                }
+
+                if (dimention % 2 != 0)
+                {
+                    Console.WriteLine("{0} should be even number.", dimentionName);
+                    return false;
+                }
+
+                if (dimention > 100)
+                {
+                    Console.WriteLine("{0} should not exceed 100", dimentionName);
+                    return false;
+                }
+
+                if (i == 0)
+                {
+                    n = dimention;
+                }
+                else
+                {
+                    m = dimention;
+                }
+            }
+
+            inputLayer = new int[n, m];
+            for (var i = 0; i < n; i++)
+            {
+                var numbersString = Console.ReadLine();
+
+                var numbers = numbersString
+                    .Split(' ')
+                    .Where(x => !string.IsNullOrEmpty(x))
+                    .ToList();
+
+                if (numbers.Count != m)
+                {
+                    Console.WriteLine("Line should contain {0} numbers but received {1}", m, numbers.Count);
+                    return false;
+                }
+
+                for (var j = 0; j < numbers.Count; j++)
+                {
+                    int intValue;
+                    if (int.TryParse(numbers[j], out intValue))
+                    {
+                        inputLayer[i, j] = intValue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Line should contain {0} integer values.", m);
+                        return false;
+                    }
+                }
+            }
+
+            /* validate the first layer of bricks. */
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    int counter = 0;
+                    if (i >= 1 && inputLayer[i, j] == inputLayer[i - 1, j])
+                    {
+                        counter++;
+                    }
+                    if (j >= 1 && inputLayer[i, j] == inputLayer[i, j - 1])
+                    {
+                        counter++;
+                    }
+                    if (i + 1 < n && inputLayer[i, j] == inputLayer[i + 1, j])
+                    {
+                        counter++;
+                    }
+                    if (j + 1 < m && inputLayer[i, j] == inputLayer[i, j + 1])
+                    {
+                        counter++;
+                    }
+
+                    if (counter > 1)
+                    {
+                        Console.WriteLine("Invalid layer of bricks!");
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
